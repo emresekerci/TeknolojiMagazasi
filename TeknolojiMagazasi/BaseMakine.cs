857e720fc0358a67ef7a2e73152e07ed6f959e63
Administrator <Administrator@DESKTOP-BEJOVSO>
2024-08-15 17:51:57 +0300

    Add project files.

---
+++ TeknolojiMagazasi/BaseMakine.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeknolojiMagazasi
+{
+
+    public abstract class BaseMakine
+    {
+        public DateTime UretimTarihi { get; }
+        public string SeriNumarasi { get; set; }
+        public string Ad { get; set; }
+        public string Aciklama { get; set; }
+        public string IsletimSistemi { get; set; }
+
+        // Constructor
+        public BaseMakine()
+        {
+            UretimTarihi = DateTime.Now;
+        }
+
+        public virtual void BilgileriYazdir()
+        {
+            Console.WriteLine($"Üretim Tarihi: {UretimTarihi}");
+            Console.WriteLine($"Seri Numarası: {SeriNumarasi}");
+            Console.WriteLine($"Ad: {Ad}");
+            Console.WriteLine($"Açıklama: {Aciklama}");
+            Console.WriteLine($"İşletim Sistemi: {IsletimSistemi}");
+        }
+        public abstract void UrunAdiGetir();
+    }
+    public class Telefon : BaseMakine
+    {
+        // Telefonun Türkiye lisanslı olup olmadığını belirten özellik
+        public bool TrLisansliMi { get; set; }
+
+        public override void BilgileriYazdir()
+        {
+            base.BilgileriYazdir();
+            Console.WriteLine($"TR Lisanslı mı: {(TrLisansliMi ? "Evet" : "Hayır")}");
+        }
+        // Telefonun adını ekrana yazdırmak için kullanılan metod
+        public override void UrunAdiGetir()
+        {
+            Console.WriteLine($"Telefonunuzun adı ---> {Ad}");
+        }
+    }
+    public class Bilgisayar : BaseMakine
+    {
+        private int _usbGirisSayisi;
+        public int UsbGirisSayisi
+        {
+            get { return _usbGirisSayisi; }
+            set
+            {
+                // USB giriş sayısının 2 veya 4 olup olmadığını kontrol eder
+                if (value == 2 || value == 4)
+                    _usbGirisSayisi = value;
+                else
+                {
+                    // Geçersiz bir değer girildiğinde uyarı mesajı verir ve varsayılan değeri -1 olarak ayarlar
+                    Console.WriteLine("Usb Giriş Sayısı sadece 2 veya 4 olabilir.");
+                    _usbGirisSayisi = -1;
+                }
+            }
+        }
+
+        public bool BluetoothVarMi { get; set; }
+        // Bilgisayar bilgilerini ekrana yazdırmak için kullanılan metod
+        public override void BilgileriYazdir()
+        {
+            base.BilgileriYazdir();
+            Console.WriteLine($"USB Giriş Sayısı: {UsbGirisSayisi}");
+            Console.WriteLine($"Bluetooth Var mı: {(BluetoothVarMi ? "Evet" : "Hayır")}");
+        }
+
+        public override void UrunAdiGetir()
+        {
+            Console.WriteLine($"Bilgisayarınızın adı ---> {Ad}");
+        }
+    }
+
+
+
+
+}
---
+++ TeknolojiMagazasi/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using TeknolojiMagazasi;
+
+
+
+//Kullanıcıya ürün seçimi yaptırılır.
+bool Sectirme = true;
+
+while (Sectirme)
+{
+    Console.WriteLine("Lütfen bir seçim yapınız:");
+    Console.WriteLine("1 - Telefon üret");
+    Console.WriteLine("2 - Bilgisayar üret");
+    Console.Write("Seçiminiz: ");
+    //Tekrar while oluşturularak şartlar belirlenir ve kullanıcının seçimine uygun döngü başlar.
+    int secim;
+    while (!int.TryParse(Console.ReadLine(), out secim) || (secim != 1 && secim != 2))
+    {
+        Console.Write("Geçersiz seçim. Lütfen 1 veya 2 giriniz: ");
+    }
+
+    if (secim == 1)
+    {
+        Telefon yeniTelefon = new Telefon();
+        BilgileriAl(yeniTelefon);
+        Console.Write("TR Lisanslı mı? (E/H): ");
+        string trLisans = Console.ReadLine().ToLower();
+        yeniTelefon.TrLisansliMi = trLisans == "e";
+
+        yeniTelefon.BilgileriYazdir();
+        yeniTelefon.UrunAdiGetir();
+    }
+    else if (secim == 2)
+    {
+        Bilgisayar yeniBilgisayar = new Bilgisayar();
+        BilgileriAl(yeniBilgisayar);
+        Console.Write("USB Giriş Sayısı (2/4): ");
+        int usbGirisSayisi;
+        while (!int.TryParse(Console.ReadLine(), out usbGirisSayisi))
+        {
+            Console.Write("Geçersiz giriş. Lütfen sayı giriniz: ");
+        }
+        yeniBilgisayar.UsbGirisSayisi = usbGirisSayisi;
+
+        Console.Write("Bluetooth Var mı? (E/H): ");
+        string bluetoothVarMi = Console.ReadLine().ToLower();
+        yeniBilgisayar.BluetoothVarMi = bluetoothVarMi == "e";
+
+        yeniBilgisayar.BilgileriYazdir();
+        yeniBilgisayar.UrunAdiGetir();
+    }
+
+    Console.Write("Başka bir ürün üretmek ister misiniz? (E/H): ");
+    string devam = Console.ReadLine().ToLower();
+    Sectirme = devam == "e";
+}
+
+Console.WriteLine("İyi günler!");
+
+
+static void BilgileriAl(BaseMakine makine)
+{
+    // Kullanıcıdan seri numarası bilgisini alır ve makine nesnesinin SeriNumarasi özelliğine atar
+    Console.Write("Seri Numarası: ");
+    makine.SeriNumarasi = Console.ReadLine();
+
+    // Kullanıcıdan ad bilgisini alır ve makine nesnesinin Ad özelliğine atar
+    Console.Write("Ad: ");
+    makine.Ad = Console.ReadLine();
+
+    // Kullanıcıdan açıklama bilgisini alır ve makine nesnesinin Aciklama özelliğine atar
+    Console.Write("Açıklama: ");
+    makine.Aciklama = Console.ReadLine();
+
+    // Kullanıcıdan işletim sistemi bilgisini alır ve makine nesnesinin IsletimSistemi özelliğine atar
+    Console.Write("İşletim Sistemi: ");
+    makine.IsletimSistemi = Console.ReadLine();
+}
